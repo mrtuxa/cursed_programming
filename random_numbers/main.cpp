b3ceed8acca7e0b3f0696f416362fb1e2a2123d0
mrtuxa <mrtuxa@leipzig.freifunk.net>
2023-03-25 14:08:15 +0100

    init project

---
+++ random_numbers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cstdlib>
+
+#include "main.h"
+
+
+a aa aaa
+
+aaaa aaaaa() {
+    aaaaaa((aaaaaaa) aaaaaaaa(aaaaaaaaa));
+
+    aaaaaaaaaa(aaaa aaaaaaaaaaa=aaaaaaaaaaaaaa; aaaaaaaaaaa<=aaaaaaaaaaaa; aaaaaaaaaaaaa) {
+        aaaa aaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaa + (aaaaaaaaaaaaaaaaaaa % aaaaaaaaaaaaaaaaaaaaa);
+
+        aaaaaaaaaaaaaaaaaaaaaa<<aaaaaaaaaaaaaaa<<aaaaaaaaaaaaaaaaaaaaaaa
+    }
+}
